7d04360f0703284a9718eb94c27c7e3277ded849
Programmer2011bird <112953035+Programmer2011bird@users.noreply.github.com>
2024-11-27 21:52:46 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+yay -S tmux
 yay -S kitty
 yay -S nekoray-bin
 yay -S telegram-desktop
 yay -S oh-my-posh
+yay -S neovim
 
 cd ~/.config
 git clone https://github.com/Programmer2011bird/dotfiles.git
