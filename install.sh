95482662c9d554c54ceec9145a2fc651aab3b455
Programmer2011bird <112953035+Programmer2011bird@users.noreply.github.com>
2024-11-27 16:12:03 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+yay -S kitty
+yay -S nekoray-bin
+yay -S telegram-desktop
+yay -S oh-my-posh
+
+cd ~/.config
+git clone https://github.com/Programmer2011bird/dotfiles.git
+mv ~/.config/dotfile/* ~/.config/
