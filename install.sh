904e31d0c10ccacefede2c2a403c02f80b7fce29
Programmer2011bird <112953035+Programmer2011bird@users.noreply.github.com>
2024-12-06 06:08:35 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
-yay -S tmux
-yay -S kitty
-yay -S nekoray-bin
-yay -S telegram-desktop
-yay -S oh-my-posh
-yay -S neovim
-yay -S npm
-
+yay -S tmux kitty nekoray-bin telegram-desktop oh-my-posh neovim npm
+yay -S grim thunar mouspad
 cd ~/.config
 git clone https://github.com/Programmer2011bird/dotfiles.git
 mv ~/.config/dotfile/* ~/.config/
