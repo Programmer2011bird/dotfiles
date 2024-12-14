41ea2467a8cd7f0717d3504a33368d07ca7e4a90
Programmer2011bird <maysamshaker7@gmail.com>
2024-12-14 05:22:01 +0330

    #

---
+++ waybar/scripts/rofi-powermenu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define options
+options=" Shutdown\n Reboot\n Lock\n Exit\n Suspend\n   Lock & Suspend"
+
+# Show the menu
+chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu")
+
+# Perform the selected action
+case "$chosen" in
+    " Shutdown")
+        systemctl poweroff
+        ;;
+    " Reboot")
+        systemctl reboot
+        ;;
+    " Lock")
+        hyprctl dispatch exec "hyprlock"
+        ;;
+    " Exit")
+        hyprctl dispatch exit
+        ;;
+    " Suspend")
+        systemctl suspend
+        ;;
+    "   Lock & Suspend")
+        hyprctl dispatch exec "hyprlock" && systemctl suspend
+        ;;
+    *)
+        # If no valid option is chosen, exit
+        exit 1
+        ;;
+esac
+
